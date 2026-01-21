24c182bf225f8dd04f60f6da594c775e706619dc
GI JEONG HONG <hgjwilly@gmail.com>
2026-01-21 22:34:55 +0900

    Setting: 프로젝트 생성
    프로젝트를 생성했습니다.
    Codebased, Clean Architecture + MVVM 구조를 만들었습니다.

---
+++ dnd-14th-1-iOS/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dnd-14th-1-iOS
+//
+//  Created by 홍기정 on 1/21/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
