054f812bef2e0d3824574bf850f46f80d09576d8
GI JEONG HONG <hgjwilly@gmail.com>
2026-02-06 09:51:01 +0900

    chore: Login-Onboarding-Home 연결
    SceneDelegate에서 로그인상태를 확인하고 Login 또는 Home 을 시작합니다.
    Login에서 Onboarding을 거쳐 Home으로 이동합니다.

---
+++ dnd-14th-1-iOS/Presentation/Home/HomeTabbarController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeTabbarController.swift
+//  dnd-14th-1-iOS
+//
+//  Created by 홍기정 on 2/6/26.
+//
+
+import UIKit
+
+final class HomeTabbarController: UITabBarController {
+    
+}
+
+
---
+++ dnd-14th-1-iOS/Presentation/Login/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  dnd-14th-1-iOS
+//
+//  Created by 홍기정 on 2/6/26.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+final class LoginViewController: UIViewController {
+    
+    let button = UIButton().then {
+        $0.setTitle("onboarding", for: .normal)
+        $0.backgroundColor = .white
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        configureView()
+    }
+    
+    @objc private func buttonTapped() {
+        let onboardingViewController = OnboardingViewController()
+        navigationController?.setViewControllers([onboardingViewController], animated: true)
+    }
+    
+    private func configureView() {
+        view.addSubview(button)
+        button.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
