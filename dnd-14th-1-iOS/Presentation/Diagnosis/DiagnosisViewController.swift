b7480bc1ac9d10278eeb06762cd7a4407b5a9e67
kwonseokki <kwons3327@gmail.com>
2026-02-11 00:37:30 +0900

    add: 진단뷰 추가

---
+++ dnd-14th-1-iOS/Presentation/Diagnosis/DiagnosisViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DiagnosisViewController.swift
+//  dnd-14th-1-iOS
+//
+//  Created by a on 2/11/26.
+//
+
+import UIKit
+
+class DiagnosisViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
