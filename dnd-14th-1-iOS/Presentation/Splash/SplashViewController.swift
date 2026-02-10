aadfdf2907dcceb6aa5daad0d8665f17fba2c712
GI JEONG HONG <hgjwilly@gmail.com>
2026-02-10 13:25:25 +0900

    design: 스플래시 UI 구현

---
+++ dnd-14th-1-iOS/Presentation/Splash/SplashViewController.swift
@@ -0,0 +1,63 @@
+//
+//  SplashViewController.swift
+//  dnd-14th-1-iOS
+//
+//  Created by 홍기정 on 2/10/26.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+final class SplashViewController: BaseViewController {
+    
+    // MARK: - UI Components
+    private let subtitleLabel = UILabel().then {
+        $0.attributedText = NSAttributedString(
+            string: "가벼운 프롬프트, 단단해지는 빙하",
+            attributes: [
+                .font : UIFont.body2_b,
+                .foregroundColor : UIColor(hexCode: "FFFFFF")
+            ]
+        )
+        $0.textAlignment = .center
+    }
+    private let logoImageView = UIImageView(image: UIImage.logotype1)
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        setUpGradientBackground()
+        super.viewDidLoad()
+    }
+    
+    private func setUpGradientBackground() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor.primary500.cgColor,
+            UIColor(hexCode: "9BD7FF").cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        view.layer.addSublayer(gradientLayer)
+    }
+    
+    override func addSubview() {
+        [subtitleLabel, logoImageView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func setLayout() {
+        logoImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        subtitleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(21)
+            $0.bottom.equalTo(logoImageView.snp.top)
+        }
+    }
+}
+
