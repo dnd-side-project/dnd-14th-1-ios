6d6b2f4efa2089da65529cf3c61807a4e590ef1c
GI JEONG HONG <hgjwilly@gmail.com>
2026-02-17 01:56:52 +0900

    add: UIViewController extension 추가
    빈 화면을 탭했을 때 키보드를 내려주는 메서드를 추가했습니다.

---
+++ dnd-14th-1-iOS/Global/Extension/UIViewController+.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+.swift
+//  dnd-14th-1-iOS
+//
+//  Created by 홍기정 on 2/17/26.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func dismissKeyboardWhenTapAround() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
