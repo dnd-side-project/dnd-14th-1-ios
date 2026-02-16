5846620e545345b31f7d94ac549fc9172dfcd24d
kwonseokki <kwons4163@naver.com>
2026-02-17 01:22:13 +0900

    add: 프롬프트 개선 뷰 추가

---
+++ dnd-14th-1-iOS/Presentation/Diagnosis/ViewController/PromptImprovedViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PromptImprovedViewController.swift
+//  dnd-14th-1-iOS
+//
+//  Created by a on 2/17/26.
+//
+
+import UIKit
+
+class PromptImprovedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
