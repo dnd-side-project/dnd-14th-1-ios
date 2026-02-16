504c9f058a6986209218b46e2b8d77675bddf1f4
kwonseokki <kwons4163@naver.com>
2026-02-17 00:47:41 +0900

    feat: 프롬프트 진단 결과 뷰 구현

--- dnd-14th-1-iOS/Presentation/Diagnosis/ViewController/DiagnosisResultViewController.swift
+++ dnd-14th-1-iOS/Presentation/Diagnosis/ViewController/DiagnosisResultViewController.swift
@@ -7,9 +7,194 @@
 
 import UIKit
 
-class DiagnosisResultViewController: UIViewController {
+import SnapKit
+import Then
 
+class DiagnosisResultViewController: BaseViewController {
+    
+    // MARK: - Properties
+    
+    private let backgroundView = UIImageView()
+    private let containerScrollView = UIScrollView()
+    private let promptEfficiencyLabel = UILabel()
+    private let promptStatusLabel = UILabel()
+    private let meltedGlacierAmountLabel = UILabel()
+    private let meltedGlacierUnitLabel = UILabel()
+    private let tokenUsageScrollView = UIScrollView()
+    private let tokenUsageStackView = UIStackView()
+    private let glacierImageView = UIImageView()
+    private let promptView = PromptView()
+    private let buttonStackView = UIStackView()
+    private let promptEditButton = AppButton(size: .large, title: "프롬프트 수정하기", image: UIImage(resource: .pencilSimpleLine))
+    private let homeButton = UIButton()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
+    }
+    
+    override func setStyle() {
+        containerScrollView.do {
+            $0.showsVerticalScrollIndicator = false
+        }
+        
+        backgroundView.do {
+            $0.image = UIImage(resource: .diagnosisResultBgWarning)
+        }
+        
+        promptEfficiencyLabel.do {
+            $0.font = .headline3_b
+            $0.textColor = .white
+            $0.text = "비효율적인 프롬프트예요!"
+        }
+        
+        promptStatusLabel.do {
+            $0.font = .body1_b
+            $0.textColor = .white
+            $0.text = "녹아내린 빙하량"
+        }
+        
+        meltedGlacierAmountLabel.do {
+            $0.font = .hakgyoansimDunggeunmisoBold_32
+            $0.textColor = .negativeDarkbg
+            $0.text = "-0.75"
+        }
+        
+        tokenUsageScrollView.do {
+            $0.alwaysBounceHorizontal = true
+            $0.alwaysBounceVertical = false
+            $0.showsHorizontalScrollIndicator = false
+        }
+        
+        tokenUsageStackView.do {
+            $0.spacing = 16
+            $0.axis = .horizontal
+        }
+        
+        glacierImageView.do {
+            $0.image = UIImage(resource: .glacier3)
+        }
+        
+        meltedGlacierUnitLabel.do {
+            $0.font = .hakgyoansimDunggeunmisoRegular_24
+            $0.textColor = .negativeDarkbg
+            $0.text = "kg"
+        }
+        
+        buttonStackView.do {
+            $0.spacing = 12
+        }
+        
+        homeButton.do {
+            $0.setImage(UIImage(resource: .homeButton), for: .normal)
+        }
+    }
+    
+    override func setLayout() {
+        backgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        containerScrollView.snp.makeConstraints {
+            $0.horizontalEdges.bottom.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        promptEfficiencyLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(containerScrollView.contentLayoutGuide).offset(20)
+        }
+        
+        promptStatusLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(promptEfficiencyLabel.snp.bottom).offset(8)
+        }
+        
+        meltedGlacierAmountLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(promptStatusLabel.snp.bottom).offset(8)
+        }
+        
+        tokenUsageScrollView.snp.makeConstraints {
+            $0.leading.equalTo(view).offset(20)
+            $0.trailing.equalTo(view)
+            $0.top.equalTo(meltedGlacierAmountLabel.snp.bottom).offset(28)
+            $0.height.equalTo(92)
+        }
+        
+        tokenUsageStackView.snp.makeConstraints {
+            $0.edges.equalTo(tokenUsageScrollView.contentLayoutGuide)
+            $0.height.equalTo(tokenUsageScrollView)
+        }
+        
+        glacierImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.top.equalTo(tokenUsageScrollView.snp.bottom).offset(22)
+        }
+        
+        // contentLayoutGuide를 설정해야 ScrollView가 스크롤할 영역을 알수있음
+        promptView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.top.equalTo(glacierImageView.snp.bottom).offset(17)
+            $0.bottom.equalTo(containerScrollView.contentLayoutGuide)
+            $0.width.equalTo(containerScrollView.frameLayoutGuide).offset(-40)
+        }
+        
+        meltedGlacierUnitLabel.snp.makeConstraints {
+            $0.centerY.equalTo(meltedGlacierAmountLabel)
+            $0.leading.equalTo(meltedGlacierAmountLabel.snp.trailing).offset(4)
+        }
+        
+        buttonStackView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-5)
+        }
+        
+        homeButton.snp.makeConstraints {
+            $0.width.equalTo(68)
+            $0.height.equalTo(60)
+        }
+    }
+    
+    override func addSubview() {
+        // view에 subview 추가
+        view.addSubviews(
+            backgroundView,
+            containerScrollView,
+            buttonStackView
+        )
+        
+        // 토큰사용량 스택뷰에 서브뷰 추가
+        tokenUsageStackView.addArrangedSubviews(
+            TokenStatView(
+                image: UIImage(resource: .tokenUsage),
+                title: "현재 토큰 사용량",
+                value: "142개"
+            ),
+            TokenStatView(
+                image: UIImage(resource: .costUsage),
+                title: "예상 손실",
+                value: "-"
+            ),
+        )
+        
+        // 토큰사용량 스크롤뷰에 스택뷰 추가
+        tokenUsageScrollView.addSubviews(
+            tokenUsageStackView
+        )
+        
+        buttonStackView.addArrangedSubviews(
+            promptEditButton,
+            homeButton
+        )
+        
+        // 컨테이너뷰에 최종 뷰 추가
+        containerScrollView.addSubviews(
+            promptEfficiencyLabel,
+            promptStatusLabel,
+            meltedGlacierAmountLabel,
+            meltedGlacierUnitLabel,
+            tokenUsageScrollView,
+            glacierImageView,
+            promptView
+        )
     }
 }
