719b71f523cadcb3fdc271c08c49e614c4297278
권석기 <kwons4163@naver.com>
2026-02-25 23:03:03 +0900

    chore: Claude API Key 추가

--- dnd-14th-1-iOS/AppConfig.swift
+++ dnd-14th-1-iOS/AppConfig.swift
@@ -14,4 +14,11 @@ struct AppConfig {
         }
         return url
     }()
+    
+    static let caludeApiKey: String = {
+        guard let apiKey = Bundle.main.infoDictionary?["CLAUDE_API_KEY"] as? String, !apiKey.isEmpty else {
+            fatalError("CLAUDE_API_KEY must be set in the Info.plist and cannot be empty.")
+        }
+        return apiKey
+    }()
 }
