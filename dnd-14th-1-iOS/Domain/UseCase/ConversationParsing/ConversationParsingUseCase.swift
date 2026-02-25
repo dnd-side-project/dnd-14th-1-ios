9147841f61c6c0029afb2aac0eefab1aa9b0f9e5
권석기 <kwons4163@naver.com>
2026-02-26 03:04:34 +0900

    feat: 대화 내용 크롤링 UseCase 추가

---
+++ dnd-14th-1-iOS/Domain/UseCase/ConversationParsing/ConversationParsingUseCase.swift
@@ -0,0 +1,35 @@
+//
+//  ConversationParsingUseCase.swift
+//  dnd-14th-1-iOS
+//
+//  Created by a on 2/26/26.
+//
+
+protocol ConversationParsingUseCase {
+    func excute(promptInput: PromptInput) async throws -> PromptDiagnosis
+}
+
+final class DefaultConversationParsingUseCase: ConversationParsingUseCase {
+    
+    private let conversationRepository: ConversationRepository
+    
+    init(conversationRepository: ConversationRepository) {
+        self.conversationRepository = conversationRepository
+    }
+    
+    func excute(promptInput: PromptInput) async throws -> PromptDiagnosis {
+        try await conversationRepository.fetchConversations()
+        return .init(
+            efficiency: .efficiency,
+            meltedGlacierAmount: 0,
+            inputToken: 0,
+            outputToken: 0,
+            estimatedLoss: 0,
+            originalPrompt: promptInput.value,
+            usingModel: .claude_haiku_4_5,
+            source: .url
+        )
+    }
+}
+
+
