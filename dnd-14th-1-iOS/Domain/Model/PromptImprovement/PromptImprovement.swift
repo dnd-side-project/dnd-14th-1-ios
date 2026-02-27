f1f5e9a4eb0d34771f78b1365ef931a76b91b446
권석기 <kwons4163@naver.com>
2026-02-27 17:54:44 +0900

    feat: 프롬프트 개선 결과 타입 추가

--- dnd-14th-1-iOS/Domain/Model/PromptImprovement/PromptImprovement.swift
+++ dnd-14th-1-iOS/Domain/Model/PromptImprovement/PromptImprovement.swift
@@ -9,5 +9,26 @@ import FoundationModels
 
 @Generable
 struct ImprovedSentences {
-    let sentences: [String]
+    let originalSentence: String
+    let improvedSetence: String
+}
+
+@Generable
+struct ImprovedPrompt {
+    @Guide(description: "개선된 프롬프트 문장")
+    var improvedPrompt: String
+    @Guide(description: "프롬프트 수정한 이유를 상세하게 설명합니다.")
+    var reason: String
+}
+
+@Generable
+struct ImprovedPromptList {
+    @Guide(description: "개선된 프롬프트 목록")
+    var items: [ImprovedPrompt]
+}
+
+struct ImprovePromptResult {
+    let originalPrompt: String
+    let improvedPrompt: String
+    let reason: String
 }
